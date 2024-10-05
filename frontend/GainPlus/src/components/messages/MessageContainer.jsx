6ea9756b7f4199f96a8b6a126527d96f38ba148d
Muskan Gupta <muskangupta260699@gmail.com>
2024-10-05 15:24:19 -0400

    image upload

--- frontend/GainPlus/src/components/messages/MessageContainer.jsx
+++ frontend/GainPlus/src/components/messages/MessageContainer.jsx
@@ -1,23 +1,123 @@
-import React from "react";
+import React, { useState } from "react";
 import Message from "./Message";
 import Messages from "./Messages";
 
 const MessageContainer = () => {
-  const styles={
-    container:{
-      // border:" 1px solid black",
-      // borderRadius:"7px",
-      // width:"95%",
-      height:"80%",
-      // marginBottom:"1px",
-      // padding:"15px",
-      // boxShadow:"5px 5px 1px 1px black",
-      // backgroundColor:"black",
-    }, 
-  };
-  return (
-    <div className="border-r border-t border-l text-md rounded-t-lg w-full p-4 bg-gray-900 text-white" style={styles.container}>
+  const styles = {
+    container: {
+      height: "80%",
+    },
+    image: {
+      width: "300px",
+      height: "250px",
+    },
+    imageStyle: {
+      border: "1px solid white",
+      width: "350px",
+      height: "350px",
+      borderStyle: "dashed",
+      display: "flex",
+      flexDirection:"column",
+      alignItems:"center",
+      justifyContent:"center",
+    },
+    imageContainer: {
+      display: "flex",
+    },
+    uploadedImageContainer:{
+      flexDirection:"column",
+    },
+    newImageUpload:{
+      display:"flex",
+      flexDirection:"column",
+      alignItems:"center",
+      justifyContent:"center",
+    },
+    imageLabel:{
+      fontSize:"30px",
+      marginBottom: "10px",
+    },
+    imageGroup:{
+      display:"flex",
+      flexDirection:"column",
+      justifyContent:"flex-end",
+      // // flexDirection:"row",
+      alignItems:"flex-end",
       
+
+    },
+    confirmLabel:{
+      marginTop:"10px",
+      textAlign:"center",
+      border:"2px solid white",
+      padding:"10px",
+      backgroundColor:"green",
+      borderRadius:"22px"
+    },
+  };
+
+  const [yourImage, setYourImage] = useState();
+  const [desiredImage, setDesiredImage] = useState();
+  const [confirm, setConfirm]= useState(false);
+  function handleYourImage(e) {
+    console.log(e.target.files);
+    setYourImage(URL.createObjectURL(e.target.files[0]));
+  };
+  function handleDesiredImage(e) {
+    console.log(e.target.files);
+    setDesiredImage(URL.createObjectURL(e.target.files[0]));
+  };
+   function handleConfirm(e){
+    setConfirm(true);
+  };
+
+  return (
+    <div
+      className="border-r border-t border-l text-md rounded-t-lg w-full p-4 bg-gray-900 text-white"
+      style={styles.container}
+    >
+      <div style={styles.imageGroup}>
+        <div style={styles.imageContainer}>
+          <div style={styles.imageStyle}>
+            {!yourImage ? (
+              <>
+              <div style={styles.newImageUpload}>
+                <h2 style={styles.imageLabel}>Your Photo</h2>
+                <input type="file" onChange={handleYourImage}></input>
+                </div>
+              </>
+            ) : (
+              <>
+              <div style={styles.uploadedImageContainer}>
+                <img style={styles.image} src={yourImage} />
+                <h2 >Re-upload Your Photo</h2>
+                <input type="file" onChange={handleYourImage}></input>
+                </div>
+              </>
+            )}
+          </div>
+          <div style={styles.imageStyle}>
+            {!desiredImage ? (
+              <>
+              <div style={styles.newImageUpload}>
+                <h2 style={styles.imageLabel}>Desired Photo</h2>
+                <input type="file" onChange={handleDesiredImage}></input>
+                </div>
+              </>
+            ) : (
+              <>
+              <div style={styles.uploadedImageContainer}>
+                <img style={styles.image} src={desiredImage} />
+                <h2>Re-upload desired Photo</h2>
+                <input type="file" onChange={handleDesiredImage}></input>
+                </div>
+              </>
+            )}
+          </div>
+          
+        </div>
+        <button title="Submit" style={styles.confirmLabel} onClick={handleConfirm}>Confirm</button>
+      </div>
       <Messages />
     </div>
   );
--- frontend/GainPlus/src/components/messages/Messages.jsx
+++ frontend/GainPlus/src/components/messages/Messages.jsx
@@ -4,10 +4,10 @@ import Message from "./Message";
 const Messages = () => {
   return (
     <div className="flex flex-col items-center justify-center overflow-scroll">
+      {/* <Message />
       <Message />
-      <Message />
-      <Message />
-      <Message />
+      <Message /> */}
+      {/* <Message /> */}
     </div>
   );
 };
