cdbc3887f203af9e83dab02148e056dff7b56ef4
Ayan Saiyed <145615216+AyanASaiyed@users.noreply.github.com>
2024-10-05 09:01:58 -0400

    Created MessageInput along with basic messages file

--- frontend/GainPlus/src/App.jsx
+++ frontend/GainPlus/src/App.jsx
@@ -1,50 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState } from "react";
 import "./App.css";
-import Home from "./pages/home";
+import Home from "./Pages/homePage/Home";
 import SignIn from "./Pages/SignIn";
 import SignUp from "./Pages/SignUp";
+import MessageInput from "./components/messages/MessageInput";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route index path="/" element={<Home></Home>}></Route>
-    //   </Routes>
-    // </BrowserRouter>
     <div className="p-4 h-screen flex items-center justify-center">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
+          {/* REMOVE THIS, ITS FOR MY TESTING ONLY*/}
+          <Route path="/input" element={<MessageInput />} />
         </Routes>
       </BrowserRouter>
     </div>
-    // <>
-    //   <div>
-    //     <a href="https://vitejs.dev" target="_blank">
-    //       <img src={viteLogo} className="logo" alt="Vite logo" />
-    //     </a>
-    //     <a href="https://react.dev" target="_blank">
-    //       <img src={reactLogo} className="logo react" alt="React logo" />
-    //     </a>
-    //   </div>
-    //   <h1>Vite + React</h1>
-    //   <div className="card">
-    //     <button onClick={() => setCount((count) => count + 1)}>
-    //       count is {count}
-    //     </button>
-    //     <p>
-    //       Edit <code>src/App.jsx</code> and save to test HMR
-    //     </p>
-    //   </div>
-    //   <p className="read-the-docs">
-    //     Click on the Vite and React logos to learn more
-    //   </p>
-    // </>
   );
 }
 
