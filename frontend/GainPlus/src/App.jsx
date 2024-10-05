e6cea67d5908ad1cdc52ec3c7c30f8c5f06a198f
Ayan Saiyed <145615216+AyanASaiyed@users.noreply.github.com>
2024-10-05 09:28:14 -0400

    updated app and home

--- frontend/GainPlus/src/App.jsx
+++ frontend/GainPlus/src/App.jsx
@@ -4,6 +4,8 @@ import Home from "./Pages/homePage/Home";
 import SignIn from "./Pages/SignIn";
 import SignUp from "./Pages/SignUp";
 import MessageInput from "./components/messages/MessageInput";
+import { auth } from "./firebase/firebase";
+import user from "./Pages/SignIn";
 
 function App() {
 
@@ -11,7 +13,7 @@ function App() {
     <div className="p-4 h-screen flex items-center justify-center">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={auth ? <Home /> : <SignIn/>} />
           <Route path="/login" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
           {/* REMOVE THIS, ITS FOR MY TESTING ONLY*/}
