33e12c41f2d09c12c29053fe8cf38547a84b2177
Muskan Gupta <muskangupta260699@gmail.com>
2024-10-04 23:53:06 -0400

    signup

--- frontend/GainPlus/src/pages/home.jsx
+++ frontend/GainPlus/src/pages/home.jsx
@@ -3,31 +3,43 @@ const Home = () => {
   const styles = {
     container: {
       border: "2px solid black",
-      padding: "20px",
+      padding: "10px",
       borderRadius: "5px",
+    },
+    heading:{
+        fontSize:"30px",
+        textAlign:"left",
     },
     formGroup: {
       display: "flex",
       flexDirection: "column",
-    
+      alignItems:"center",
+    },
+    label:{
+        fontSize:"25px",
+        padding:"5px",
+    },
+    input:{
+        fontSize:"25px",
     },
   };
   return (
     <section>
-      <h2> Welcome!!</h2>
+      <h2 style={styles.heading}> Welcome!!</h2>
       <form style={styles.container}>
-        <legend>Sign Up</legend>
+        <legend style={styles.heading}>Sign Up</legend>
         <fieldset>
           <div style={styles.formGroup}>
             <label htmlFor="email" style={styles.label}>
               Email
             </label>
-            <input type="text" id="email" />
-
+            <input type="text" id="email" style={styles.input}/>
+            </div>
+            <div style={styles.formGroup}>
             <label htmlFor="password" style={styles.label}>
               Password
             </label>
-            <input type="password" id="password" />
+            <input type="password" id="password" style={styles.input}/>
           </div>
         </fieldset>
       </form>
