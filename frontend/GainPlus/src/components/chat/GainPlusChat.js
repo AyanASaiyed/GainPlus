b1f52422c6cb88f0c811736a4228ba7aa4b32f5e
Ayan Saiyed <145615216+AyanASaiyed@users.noreply.github.com>
2024-10-06 09:18:24 -0400

    corsjson

--- frontend/GainPlus/src/components/chat/GainPlusChat.js
+++ frontend/GainPlus/src/components/chat/GainPlusChat.js
@@ -1,22 +1,22 @@
-// import axios from "axios";
+import axios from "axios";
 
-// const chatbotEndpoint = "https://dbc-9b555a31-b662.cloud.databricks.com/serving-endpoints/agents_gainplus-default-gainplus-coach_model/invocations";
+const chatbotEndpoint = "https://dbc-9b555a31-b662.cloud.databricks.com/serving-endpoints/agents_gainplus-default-gainplus-coach_model/invocations";
 
-// export const getChatbotResponse = async (message) => {
-//     try {
-//         const response = await axios.post(chatbotEndpoint, {
-//             llm_endpoint: "databricks-meta-llama-3-1-70b-instruct",
-//             warehouse_id: "9789d6ff88d9eaf8" ,
-//             model_name: "gainplus-default-gainplus-coach_model_1",
-//       inputs: { message: message },
-//     }, {
-//       headers: {
-//         Authorization: `Bearer ${apiKey}`,
-//       },
-//     });
-//     console.log("Chatbot Response: ", response.data);
-//     return response.data;
-//   } catch (error) {
-//     console.error("Error sending message to chatbot:", error);
-//   }
-// };
+export const getChatbotResponse = async (message) => {
+    try {
+        const response = await axios.post(chatbotEndpoint, {
+            llm_endpoint: "databricks-meta-llama-3-1-70b-instruct",
+            warehouse_id: "9789d6ff88d9eaf8" ,
+            model_name: "gainplus-default-gainplus-coach_model_1",
+      inputs: { message: message },
+    }, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      },
+    });
+    console.log("Chatbot Response: ", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error sending message to chatbot:", error);
+  }
+};
