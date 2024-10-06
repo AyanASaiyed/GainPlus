aad2481404e8f57d77ce94d5c3ad793d3420d5d2
Muskan Gupta <muskangupta260699@gmail.com>
2024-10-05 21:01:36 -0400

    chatBot Images use effect

--- frontend/GainPlus/src/components/messages/MessageContainer.jsx
+++ frontend/GainPlus/src/components/messages/MessageContainer.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Message from "./Message";
 import Messages from "./Messages";
-
+import { imageDb } from "../../firebase/firebase";
+import { v4 } from "uuid";
+import { getDownloadURL, listAll, ref, uploadBytes } from "firebase/storage";
 const MessageContainer = () => {
   const styles = {
     container: {
@@ -17,58 +19,109 @@ const MessageContainer = () => {
       height: "350px",
       borderStyle: "dashed",
       display: "flex",
-      flexDirection:"column",
-      alignItems:"center",
-      justifyContent:"center",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
     },
     imageContainer: {
       display: "flex",
     },
-    uploadedImageContainer:{
-      flexDirection:"column",
+    uploadedImageContainer: {
+      flexDirection: "column",
     },
-    newImageUpload:{
-      display:"flex",
-      flexDirection:"column",
-      alignItems:"center",
-      justifyContent:"center",
+    newImageUpload: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
     },
-    imageLabel:{
-      fontSize:"30px",
+    imageLabel: {
+      fontSize: "30px",
       marginBottom: "10px",
     },
-    imageGroup:{
-      display:"flex",
-      flexDirection:"column",
-      justifyContent:"flex-end",
+    imageGroup: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "flex-end",
       // // flexDirection:"row",
-      alignItems:"flex-end",
-      
-
+      alignItems: "flex-end",
     },
-    confirmLabel:{
-      marginTop:"10px",
-      textAlign:"center",
-      border:"2px solid white",
-      padding:"10px",
-      backgroundColor:"green",
-      borderRadius:"22px"
+    confirmLabel: {
+      marginTop: "10px",
+      textAlign: "center",
+      border: "2px solid white",
+      padding: "10px",
+      backgroundColor: "green",
+      borderRadius: "22px",
     },
   };
 
   const [yourImage, setYourImage] = useState();
   const [desiredImage, setDesiredImage] = useState();
-  const [confirm, setConfirm]= useState(false);
-  function handleYourImage(e) {
-    console.log(e.target.files);
-    setYourImage(URL.createObjectURL(e.target.files[0]));
-  };
-  function handleDesiredImage(e) {
-    console.log(e.target.files);
-    setDesiredImage(URL.createObjectURL(e.target.files[0]));
-  };
-   function handleConfirm(e){
+  const [yourImageFile, setYourImageFile] = useState();
+  const [desiredImageFile, setDesiredImageFile] = useState();
+  const [confirm, setConfirm] = useState(false);
+  // function handleYourImage(e) {
+  //   console.log(e.target.files);
+  //   setYourImage(URL.createObjectURL(e.target.files[0]));
+  // }
+  // function handleDesiredImage(e) {
+  //   console.log(e.target.files);
+  //   setDesiredImage(URL.createObjectURL(e.target.files[0]));
+  // }
+
+  useEffect(() => {
+    const imageFetching = async () => {
+      try {
+        const yourImageRef = ref(imageDb, "yourImage");
+        const yourImageList= await listAll(yourImageRef);
+        if(yourImageList.items.length>0){
+        const yourImageFound = await getDownloadURL(yourImageList.items[0]);
+        setYourImage(yourImageFound);
+        }
+
+        const desiredImageRef = ref(imageDb, "desiredImage");
+        const desiredImageList= await listAll(desiredImageRef);
+        if(desiredImageList.items.length>0){
+        const desiredImageFound = await getDownloadURL( desiredImageList.items[0]);
+        setDesiredImage(desiredImageFound);
+        }
+
+        if(yourImageList.items.length>0 && desiredImageList.items.length>0)
+          {
+            setConfirm(true);
+
+          }
+      } catch (error) {
+        console.log("errror");
+      }
+    };
+
+    imageFetching();
+  },[]);
+
+  const handleConfirm = async () => {
     setConfirm(true);
+    if (yourImageFile) {
+      const yourImageRef = ref(
+        imageDb,
+        `yourImage/${yourImageFile.name + v4()}`
+      );
+      await uploadBytes(yourImageRef, yourImageFile);
+      const yourImageURL = await getDownloadURL(yourImageRef);
+      setYourImage(yourImageURL);
+    }
+    if (desiredImageFile) {
+      const desiredImageRef = ref(
+        imageDb,
+        `desiredImage/${desiredImageFile.name + v4()}`
+      );
+      await uploadBytes(desiredImageRef, desiredImageFile);
+      const desiredImageURL = await getDownloadURL(desiredImageRef);
+      setDesiredImage(desiredImageURL);
+    }
+
+    //  uploadBytes(imgRef, yourImage)
   };
 
   return (
@@ -81,17 +134,27 @@ const MessageContainer = () => {
           <div style={styles.imageStyle}>
             {!yourImage ? (
               <>
-              <div style={styles.newImageUpload}>
-                <h2 style={styles.imageLabel}>Your Photo</h2>
-                <input type="file" onChange={handleYourImage}></input>
+                <div style={styles.newImageUpload}>
+                  <h2 style={styles.imageLabel}>Your Photo</h2>
+                  <input
+                    type="file"
+                    onChange={(e) => setYourImageFile(e.target.files[0])}
+                  ></input>
                 </div>
               </>
             ) : (
               <>
-              <div style={styles.uploadedImageContainer}>
-                <img style={styles.image} src={yourImage} />
-                <h2 >Re-upload Your Photo</h2>
-                <input type="file" onChange={handleYourImage}></input>
+                <div style={styles.uploadedImageContainer}>
+                  <img style={styles.image} src={yourImage} />
+                  {!confirm && (
+                    <>
+                      <h2>Re-upload Your Photo</h2>
+                      <input
+                        type="file"
+                        onChange={(e) => setYourImageFile(e.target.files[0])}
+                      ></input>
+                    </>
+                  )}
                 </div>
               </>
             )}
@@ -99,24 +162,41 @@ const MessageContainer = () => {
           <div style={styles.imageStyle}>
             {!desiredImage ? (
               <>
-              <div style={styles.newImageUpload}>
-                <h2 style={styles.imageLabel}>Desired Photo</h2>
-                <input type="file" onChange={handleDesiredImage}></input>
+                <div style={styles.newImageUpload}>
+                  <h2 style={styles.imageLabel}>Desired Photo</h2>
+                  <input
+                    type="file"
+                    onChange={(e) => setDesiredImageFile(e.target.files[0])}
+                  ></input>
                 </div>
               </>
             ) : (
               <>
-              <div style={styles.uploadedImageContainer}>
-                <img style={styles.image} src={desiredImage} />
-                <h2>Re-upload desired Photo</h2>
-                <input type="file" onChange={handleDesiredImage}></input>
+                <div style={styles.uploadedImageContainer}>
+                  <img style={styles.image} src={desiredImage} />
+                  {!confirm && (
+                    <>
+                      <h2>Re-upload desired Photo</h2>
+                      <input
+                        type="file"
+                        onChange={(e) => setDesiredImageFile(e.target.files[0])}
+                      ></input>
+                    </>
+                  )}
                 </div>
               </>
             )}
           </div>
-          
         </div>
-        <button title="Submit" style={styles.confirmLabel} onClick={handleConfirm}>Confirm</button>
+        {!confirm && (
+          <button
+            title="Submit"
+            style={styles.confirmLabel}
+            onClick={handleConfirm}
+          >
+            Confirm
+          </button>
+        )}
       </div>
       <Messages />
     </div>
